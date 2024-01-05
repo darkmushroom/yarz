3f16248fca0fec1c61c484fd87532cd2fa124cc7
darkmushroom <mensing.alexander@gmail.com>
2024-01-04 21:56:36 -0500

    simple game logic loop

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ const int SCREEN_HEIGHT = 480;
 const int TILE_SIZE = 32;
 
 const int HERO = 0;
-const int ENEMY1 = 1;
+const int LEGGY = 1;
 const int FLOOR = 0;
 
 struct RenderTarget {
@@ -39,10 +39,11 @@ bool initSDL2(SDL_Window *);
 SDL_Surface* loadSpritemap(const char *, SDL_PixelFormat *);
 SDL_Window* initWindow(SDL_Window *);
 bool initImageEngine(SDL_Window *);
-void cleanup(SDL_Window *);
 void generateTerrain(SDL_Surface *, SDL_Surface *);
 void placeTile(SDL_Surface *, int, int, int, int, SDL_Surface *);
 void place(struct Critter, SDL_Surface *);
+void gameLogic(struct Critter *, struct Critter **, int);
+void cleanup(SDL_Window *);
 
 int main(int argc, char *args[])
 {
@@ -52,12 +53,91 @@ int main(int argc, char *args[])
     if (renderTarget.screenSurface == NULL || renderTarget.window == NULL) return EXIT_FAILURE;
 
     // FIXME: may want to remap alpha color to something other than black
+    // FIXME: doesn't check to make sure files are there
     SDL_Surface *spritemap = loadSpritemap("assets/yarz-sprites.png", renderTarget.screenSurface->format);
     SDL_Surface *terrainmap = loadSpritemap("assets/yarz-terrain.png", renderTarget.screenSurface->format);
 
     struct Critter hero = {.srcSpritemap = spritemap, .spriteID = HERO, .x = 0, .y = 0 };
-    struct Critter enemy1 = {.srcSpritemap = spritemap, .spriteID = ENEMY1, .x = 32, .y = 32 };
+    struct Critter leggy = {.srcSpritemap = spritemap, .spriteID = LEGGY, .x = 128, .y = 128 };
+    struct Critter leggy2 = {.srcSpritemap = spritemap, .spriteID = LEGGY, .x = 256, .y = 256 }; // FIXME: lazy enemy copy
 
+    struct Critter *enemyList[] = {&leggy, &leggy2};
+
+    SDL_Event e;
+    while (true) {
+
+        // hack to watch for a user requested quit without depleting the event queue
+        // TODO: All events, including gracefully closing the game, should probably be handled in the same place
+        SDL_PumpEvents();
+        if (SDL_PeepEvents(&e, 1, SDL_GETEVENT, SDL_QUIT, SDL_QUIT) == 1) break;
+
+        gameLogic(&hero, enemyList, 2);
+
+        generateTerrain(terrainmap, renderTarget.screenSurface);
+        place(hero, renderTarget.screenSurface);
+        place(leggy, renderTarget.screenSurface);
+        place(leggy2, renderTarget.screenSurface); // FIXME: lazy enemy copy
+        SDL_UpdateWindowSurface(renderTarget.window);
+    }
+
+
+    SDL_FreeSurface(spritemap);
+    SDL_FreeSurface(terrainmap);
+    cleanup(renderTarget.window); // screenSurface also gets freed here, see SDL_DestroyWindow
+    return EXIT_SUCCESS;
+}
+
+void gameLogic(struct Critter *hero, struct Critter *enemyList[], int totalEntities) {
+    gameState = PLAY;
+
+    //handle hero logic
+    if (hero->x < enemyList[0]->x) {
+        hero->x += TILE_SIZE;
+    }
+    else {
+        hero->x -= TILE_SIZE;
+    }
+    if (hero->y < enemyList[0]->y) {
+        hero->y += TILE_SIZE;
+    }
+    else {
+        hero->y -= TILE_SIZE;
+    }
+
+    //handle enemy logic
+        SDL_Event e;
+        bool input_received = false;
+        while(input_received == false) {
+            SDL_PollEvent(&e);
+            if (e.type == SDL_KEYDOWN) {
+                switch(e.key.keysym.sym) {
+                    case SDLK_UP:
+                    enemyList[0]->y -= 32;
+                    input_received = true;
+                    break;
+
+                    case SDLK_DOWN:
+                    enemyList[0]->y += 32;
+                    input_received = true;
+                    break;
+
+                    case SDLK_LEFT:
+                    enemyList[0]->x -= 32;
+                    input_received = true;
+                    break;
+
+                    case SDLK_RIGHT:
+                    enemyList[0]->x += 32;
+                    input_received = true;
+                    break;
+                }
+            }
+        }
+    return;
+}
+
+/*
+void handleEvents() {
     SDL_Event e;
     while (true) {
         SDL_PollEvent(&e);
@@ -83,19 +163,9 @@ int main(int argc, char *args[])
                 break;
             }
         }
-
-        generateTerrain(terrainmap, renderTarget.screenSurface);
-        place(hero, renderTarget.screenSurface);
-        place(enemy1, renderTarget.screenSurface);
-        SDL_UpdateWindowSurface(renderTarget.window);
     }
-
-    SDL_FreeSurface(spritemap);
-    SDL_FreeSurface(terrainmap);
-    cleanup(renderTarget.window); // screenSurface also gets freed here, see SDL_DestroyWindow
-    return EXIT_SUCCESS;
 }
-
+ */
 // currently just makes a room with four walls
 void generateTerrain(SDL_Surface *terrainmap, SDL_Surface *dst) {
     enum tileset {
