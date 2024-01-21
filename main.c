cb18e372c06f849a56704dcef0e1c32f052a97ff
darkmushroom <mensing.alexander@gmail.com>
2024-01-20 23:01:06 -0500

    generate arbitrary sized maps, expand debug info, add backdrop to render avoiding smeared pixels

--- main.c
+++ main.c
@@ -6,15 +6,9 @@
 #include <stdbool.h>
 #include <string.h>
 
-/* TODO: ideally all of this will be dynamic.
- * Dynamically sized levels and a dynamically resized screen
- */
 const int INITIAL_SCREEN_WIDTH = 640;
 const int INITIAL_SCREEN_HEIGHT = 480;
 const int TILE_SIZE = 32;
-
-const int LEVEL_WIDTH = 60;
-const int LEVEL_HEIGHT = 45;
 
 const int CAVE_WALL_PROBABILITY = 45; // int percentage, so 50 = 50%
 const int CAVE_GENERATOR_ITERATIONS = 5;
@@ -45,6 +39,13 @@ struct Resources {
     SDL_Surface *icons;
     TTF_Font *gameFont;
     struct Critter *entityList;
+};
+
+// map height and width are in tiles. aka 1 = TILE_SIZE pixels
+struct GameMap {
+    int map_width;
+    int map_height;
+    int **map_array;
 };
 
 struct GameState {
@@ -81,14 +82,15 @@ enum inputs {
     DOWN,
     DOWN_LEFT,
     LEFT,
-    SKIP
+    SKIP,
+    DEBUG_GENERATE_NEW_MAP
 };
 
-int init(struct RenderTarget *, struct Resources *, struct GameState *);
-int generateMap(int ***);
+int init(struct RenderTarget *, struct Resources *, struct GameState *, struct GameMap *);
+int generateMap(struct GameMap *);
 SDL_Surface* loadSpritemap(const char *, SDL_PixelFormat *);
-void generateTerrain(int***);
-void renderTerrain(SDL_Surface *, int***, SDL_Surface *);
+void generateTerrain(struct GameMap *);
+void renderTerrain(SDL_Surface *, struct GameMap *, SDL_Surface *);
 void placeTile(SDL_Surface *, int, int, int, int, SDL_Surface *);
 void place(struct Critter, SDL_Surface *);
 void processInputs(SDL_Event *, struct GameState *, struct RenderTarget *, struct Camera *, bool *, bool *);
@@ -96,8 +98,8 @@ void gameUpdate(struct GameState *, struct Resources *, int);
 int randomRange(int, int);
 void shuffleTurnOrder(int**, int);
 //void renderDirectionIcon(SDL_Surface *, struct Critter *[], SDL_Surface *);
-SDL_Surface* updateDebugInfo(TTF_Font *, struct RenderTarget *, int);
-void asciiOutputMap(int**);
+SDL_Surface* updateDebugInfo(TTF_Font *, struct RenderTarget *, struct GameMap *, int);
+void asciiOutputMap(struct GameMap *);
 void cleanup(SDL_Window *);
 
 // TODO: should seed RNG
@@ -106,26 +108,25 @@ int main(int argc, char *args[])
 {
     struct GameState gameState = { .lastInput = NONE, .endTurn = false, .status = INIT, .currentPlayer = 0, .currentTurn = 0 };
     struct Camera camera = { .cameraX = 0, .cameraY = 0, .cameraScale = 0 };
+    struct GameMap gameMap = {.map_height = randomRange(50, 100), .map_width = randomRange(50, 100), .map_array = NULL };
     bool resizing = false;
     bool debugInfoChanged = false;
     struct RenderTarget renderTarget;
     struct Resources resources;
+    SDL_Event e;
 
-    init(&renderTarget, &resources, &gameState);
-
-    int **map = NULL;
-    generateMap(&map);
-    generateTerrain(&map);
+    generateMap(&gameMap);
+    generateTerrain(&gameMap);
+    init(&renderTarget, &resources, &gameState, &gameMap);
 
     shuffleTurnOrder(&gameState.turnOrder, 3);
 
     gameState.status = PLAYER_TURN;
-    SDL_Surface *debugInfo = updateDebugInfo(resources.gameFont, &renderTarget, camera.cameraScale);
+    SDL_Surface *debugInfo = updateDebugInfo(resources.gameFont, &renderTarget, &gameMap, camera.cameraScale);
     SDL_Rect debugTextRect = {.h = debugInfo->h, .w = debugInfo->w, .x = 0, .y = 0};
 
-    // make space for variables to reuse in main game loop:
-    SDL_Event e;
-
+    SDL_Surface *backdrop = SDL_CreateRGBSurfaceWithFormat(0, renderTarget.screenWidth, renderTarget.screenHeight, renderTarget.screenSurface->format->BitsPerPixel, renderTarget.screenSurface->format->format);
+    SDL_FillRect(backdrop, NULL, 0);
     while (gameState.status != EXITING) {
         processInputs(&e, &gameState, &renderTarget, &camera, &debugInfoChanged, &resizing);
 
@@ -133,12 +134,25 @@ int main(int argc, char *args[])
         if (resizing == true) {
             SDL_FreeSurface(renderTarget.screenSurface);
             renderTarget.screenSurface = SDL_GetWindowSurface(renderTarget.window);
+            SDL_FreeSurface(backdrop);
+            backdrop = SDL_CreateRGBSurfaceWithFormat(0, renderTarget.screenWidth, renderTarget.screenHeight, renderTarget.screenSurface->format->BitsPerPixel, renderTarget.screenSurface->format->format);
             resizing = false;
+        }
+
+        if (gameState.lastInput == DEBUG_GENERATE_NEW_MAP) {
+            generateMap(&gameMap);
+            generateTerrain(&gameMap);
+            SDL_FreeSurface(resources.level);
+            resources.level = SDL_CreateRGBSurfaceWithFormat(0, gameMap.map_width * TILE_SIZE, gameMap.map_height * TILE_SIZE, renderTarget.screenSurface->format->BitsPerPixel, renderTarget.screenSurface->format->format);
+            debugInfoChanged = true;
+            gameState.lastInput = NONE;
         }
 
         gameUpdate(&gameState, &resources, 3);
 
-        renderTerrain(resources.terrain, &map, resources.level);
+        SDL_BlitSurface(backdrop, NULL, renderTarget.screenSurface, NULL);
+
+        renderTerrain(resources.terrain, &gameMap, resources.level);
 
 
         // if (lastInput != NONE && endTurn == false) renderDirectionIcon(renderTarget.icons, gameState.entityList, renderTarget.level);
@@ -152,7 +166,7 @@ int main(int argc, char *args[])
 
         if (debugInfoChanged) {
             SDL_FreeSurface(debugInfo);
-            debugInfo = updateDebugInfo(resources.gameFont, &renderTarget, camera.cameraScale);
+            debugInfo = updateDebugInfo(resources.gameFont, &renderTarget, &gameMap, camera.cameraScale);
             debugTextRect.h = debugInfo->h;
             debugTextRect.w = debugInfo->w;
             debugInfoChanged = false;
@@ -172,9 +186,19 @@ int main(int argc, char *args[])
     return EXIT_SUCCESS;
 }
 
-SDL_Surface * updateDebugInfo(TTF_Font *font, struct RenderTarget *renderTarget, int cameraScale) {
-    char debugCameraText[100];
-    snprintf(debugCameraText, 100,"  scale value: %d\n scaled width: %d\nscaled height: %d", cameraScale, renderTarget->screenWidth + (cameraScale * (renderTarget->screenWidth/100)), renderTarget->screenHeight + (cameraScale * (renderTarget->screenHeight/100)));
+SDL_Surface * updateDebugInfo(TTF_Font *font, struct RenderTarget *renderTarget, struct GameMap *gameMap, int cameraScale) {
+    char debugCameraText[200];
+    snprintf(debugCameraText, 200,"   resolution: %dx%d\n  scale value: %d\n scaled width: %d\nscaled height: %d\n              tiles/ px\n    map width: (%d) %d\n   map height: (%d) %d",
+                                     renderTarget->screenWidth,
+                                     renderTarget->screenHeight,
+                                     cameraScale,
+                                     renderTarget->screenWidth + (cameraScale * (renderTarget->screenWidth/100)),
+                                     renderTarget->screenHeight + (cameraScale * (renderTarget->screenHeight/100)),
+                                     gameMap->map_width,
+                                     gameMap->map_width * TILE_SIZE,
+                                     gameMap->map_height,
+                                     gameMap->map_height * TILE_SIZE);
+
     SDL_Color yellow = {.r = 227, .g = 227, .b = 18};
     return TTF_RenderUTF8_Solid_Wrapped(font, debugCameraText, yellow, 0);
 }
@@ -356,6 +380,10 @@ void processInputs(SDL_Event *e, struct GameState *gamestate, struct RenderTarge
                 gamestate->endTurn = true;
                 break;
 
+                case SDLK_n:
+                gamestate->lastInput = DEBUG_GENERATE_NEW_MAP;
+                break;
+
                 // TODO: change these keys to fully qualified camera events (zoom in, zoom out etc)
                 case SDLK_w:
                 camera->cameraY -= 5;
@@ -398,7 +426,7 @@ void processInputs(SDL_Event *e, struct GameState *gamestate, struct RenderTarge
 // TODO: Generate more than one kind of terrain
 // "simple" cave generation based on a naive implementation of the following:
 // https://www.roguebasin.com/index.php?title=Cellular_Automata_Method_for_Generating_Random_Cave-Like_Levels
-void generateTerrain(int ***map) {
+void generateTerrain(struct GameMap *gameMap) {
 
     enum {
         FLOOR,
@@ -408,14 +436,14 @@ void generateTerrain(int ***map) {
     // first, randomize map area
     // TODO: made this map area smaller by 1 because my ass is lazy..
     // ..I mean to avoid out-of-bounds errors during generation
-    for (int i = 1; i < LEVEL_WIDTH - 1; i++) {
-        for (int j = 1; j < LEVEL_HEIGHT - 1; j++) {
+    for (int i = 1; i < gameMap->map_width - 1; i++) {
+        for (int j = 1; j < gameMap->map_height - 1; j++) {
             int roll = randomRange(0, 100);
             if (roll < CAVE_WALL_PROBABILITY) {
-                (*map)[i][j] = 1;
+                (gameMap->map_array)[i][j] = 1;
             }
             else {
-                (*map)[i][j] = 0;
+                (gameMap->map_array)[i][j] = 0;
             }
         }
     }
@@ -424,19 +452,19 @@ void generateTerrain(int ***map) {
     // For each location on the map, it analyzes the 8 adjacent squares.
     // if those squares contain a wall, we add 1 to our count
     for (int step = 0; step < CAVE_GENERATOR_ITERATIONS; step++) {
-        for (int i = 1; i < LEVEL_WIDTH - 1; i++) {
-            for (int j = 1; j < LEVEL_HEIGHT - 1; j++) {
+        for (int i = 1; i < gameMap->map_width - 1; i++) {
+            for (int j = 1; j < gameMap->map_height - 1; j++) {
                 int wall_count = 0;
                 for (int k = 0; k < 3; k++) {
                     for (int l = 0; l < 3; l++) {
-                        if ((*map)[i + k - 1][j + l - 1] == WALL && !(k == 1 && l == 1)) wall_count++;
+                        if ((gameMap->map_array)[i + k - 1][j + l - 1] == WALL && !(k == 1 && l == 1)) wall_count++;
                     }
                 }
 
                 // if our tile is a flimsy unsupported wall has less than four walls around it, we collapse it into a floor tile
                 // if instead our tile is a floor encroached by more than 4 walls, we fill this tile with a wall
-                if ((*map)[i][j] == WALL && wall_count < 4) (*map)[i][j] = FLOOR;
-                if ((*map)[i][j] == FLOOR && wall_count > 4) (*map)[i][j] = WALL;
+                if ((gameMap->map_array)[i][j] == WALL && wall_count < 4) (gameMap->map_array)[i][j] = FLOOR;
+                if ((gameMap->map_array)[i][j] == FLOOR && wall_count > 4) (gameMap->map_array)[i][j] = WALL;
             }
         }
     }
@@ -445,17 +473,17 @@ void generateTerrain(int ***map) {
 }
 
 // strictly for debugging purposes
-void asciiOutputMap(int **map) {
+void asciiOutputMap(struct GameMap *gameMap) {
 
-    for (int i = 0; i < LEVEL_WIDTH; i++) {
-        for (int j = 0; j < LEVEL_HEIGHT; j++) {
-            printf("%d", map[i][j]);
+    for (int i = 0; i < gameMap->map_width; i++) {
+        for (int j = 0; j < gameMap->map_height; j++) {
+            printf("%d", gameMap->map_array[i][j]);
         }
         printf("\n");
     }
 }
 
-void renderTerrain(SDL_Surface *terrainmap, int ***map, SDL_Surface *dst) {
+void renderTerrain(SDL_Surface *terrainmap, struct GameMap *gameMap, SDL_Surface *dst) {
     enum tileset {
         CAVE,
         FLOOR,
@@ -467,12 +495,12 @@ void renderTerrain(SDL_Surface *terrainmap, int ***map, SDL_Surface *dst) {
         EAST,
         WEST
     };
-    for (int i = 0; i < LEVEL_WIDTH; i++) {
-        for (int j = 0; j < LEVEL_HEIGHT; j++) {
-            if ((*map)[i][j] == 0) {
+    for (int i = 0; i < gameMap->map_width; i++) {
+        for (int j = 0; j < gameMap->map_height; j++) {
+            if ((gameMap->map_array)[i][j] == 0) {
                 placeTile(terrainmap, FLOOR, 0, i * 32, j * 32, dst);
             }
-            if ((*map)[i][j] == 1) {
+            if ((gameMap->map_array)[i][j] == 1) {
                 placeTile(terrainmap, CAVE, 0, i * 32, j * 32, dst);
             }
         }
@@ -540,7 +568,7 @@ int randomRange(int min, int max) {
     return min + rand() / (RAND_MAX / (max - min + 1) + 1);
 }
 
-int init(struct RenderTarget *renderTarget, struct Resources *resources, struct GameState *gameState) {
+int init(struct RenderTarget *renderTarget, struct Resources *resources, struct GameState *gameState, struct GameMap *gameMap) {
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL not initialized! SDL_Error: %s\n", SDL_GetError());
@@ -596,7 +624,7 @@ int init(struct RenderTarget *renderTarget, struct Resources *resources, struct 
         return -1;
     }
 
-    resources->gameFont = TTF_OpenFont("assets/MajorMonoDisplay-Regular.ttf", 24);
+    resources->gameFont = TTF_OpenFont("assets/MajorMonoDisplay-Regular.ttf", 14);
     if (resources->gameFont == NULL) {
         printf("Could not open font MajorMonoDisplay-Regular! TTF_Error: %s\n", TTF_GetError());
 
@@ -607,7 +635,7 @@ int init(struct RenderTarget *renderTarget, struct Resources *resources, struct 
 
     // this monster of a declaration basically says 'give me a surface the size of the level in the same format as the screen'
     // the other surfaces are optimized to the screen format on load
-    resources->level = SDL_CreateRGBSurfaceWithFormat(0, LEVEL_WIDTH * TILE_SIZE, LEVEL_HEIGHT * TILE_SIZE, renderTarget->screenSurface->format->BitsPerPixel, renderTarget->screenSurface->format->format);
+    resources->level = SDL_CreateRGBSurfaceWithFormat(0, gameMap->map_width * TILE_SIZE, gameMap->map_height * TILE_SIZE, renderTarget->screenSurface->format->BitsPerPixel, renderTarget->screenSurface->format->format);
     if (resources->level == NULL) {
         printf("Could not create level surface! SDL_Error: %s\n", SDL_GetError());
         cleanup(renderTarget->window);
@@ -631,24 +659,30 @@ int init(struct RenderTarget *renderTarget, struct Resources *resources, struct 
     return 0;
 }
 
-int generateMap(int ***map) {
+int generateMap(struct GameMap *gameMap) {
 
-    if (*map != NULL) {
-        for (int i = 0; i < LEVEL_WIDTH; i++) {
-            free((*map)[i]);
+    //free up the old map
+    if (gameMap->map_array != NULL) {
+        for (int i = 0; i < gameMap->map_width; i++) {
+            free((gameMap->map_array)[i]);
         }
-        free(*map);
+        free(gameMap->map_array);
     }
+
+    // randomize map size
+    gameMap->map_width = randomRange(50, 100);
+    gameMap->map_height = randomRange(50, 100);
+
     // allocating all the space for our map
-    *map = (int**) malloc(sizeof(int*) * LEVEL_WIDTH);
-    for (int i = 0; i < LEVEL_WIDTH; i++) {
-        (*map)[i] = (int *)malloc(sizeof(int) * LEVEL_HEIGHT);
+    gameMap->map_array = (int**) malloc(sizeof(int*) * gameMap->map_width);
+    for (int i = 0; i < gameMap->map_width; i++) {
+        (gameMap->map_array)[i] = (int *)malloc(sizeof(int) * gameMap->map_height);
     }
 
     // init everything to '1'
-    for (int i = 0; i < LEVEL_WIDTH; i++) {
-        for (int j = 0; j < LEVEL_HEIGHT; j++) {
-            (*map)[i][j] = 1;
+    for (int i = 0; i < gameMap->map_width; i++) {
+        for (int j = 0; j < gameMap->map_height; j++) {
+            (gameMap->map_array)[i][j] = 1;
         }
     }
     return 0;
